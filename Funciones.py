9d00d2f6db02bd242202bf5a2da574ff03ecc3ec
José Miguel Calderon Frutos <josemiguelcalderonfrutos@gmail.com>
2020-03-01 12:39:42 +0100

    Creacion de la segunda funcion

--- Funciones.py
+++ Funciones.py
@@ -1,2 +1,5 @@
 def ListarPelis(doc):
-    
+    return {"titulos":doc.xpath("/movies/movie/title/text()"),"años":"/movies/movie/year/text()","genero":doc.xpath("/movies/movie/genre/text()")}
+
+def NPeliculas_por_año(doc,fechaino,fechafin):
+    return len(doc.xpath("/movies/movie[year/text()>=%i and year/text()<=%i]/year/text()"))
